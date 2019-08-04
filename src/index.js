3c751920d7de8dca641d46f6ec8535fbbe478e0b
Atichat Lappanopakon <boss_atln@outlook.com>
2019-08-04 22:02:16 +0700

    Install & include date-fns to index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
-const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}]
+const { format } = require('date-fns')
+
+const date = format(new Date(), "YYYY-MM-DD [at] HH:mm")
+const data = [{name: date, uv: 400, pv: 2400, amt: 2400}]
 
 const renderLineChart = (
   <LineChart width={600} height={300} data={data}>
