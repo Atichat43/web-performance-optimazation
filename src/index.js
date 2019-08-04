fc69a0a7ed7a5fd96651f4cff3fc6bfc75e06c91
Atichat Lappanopakon <boss_atln@outlook.com>
2019-08-04 21:46:11 +0700

    Install & Include rechart demo to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}]
+
+const renderLineChart = (
+  <LineChart width={600} height={300} data={data}>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+    <CartesianGrid stroke="#ccc" />
+    <XAxis dataKey="name" />
+    <YAxis />
+  </LineChart>
+);
 
 const App = () => {
-  return <div>Hello React!</div>;
+  return <div>{renderLineChart}</div>
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
