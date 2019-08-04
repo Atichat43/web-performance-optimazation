d10cf30501a8ce7691e7ae5514e2239904d64785
Atichat Lappanopakon <boss_atln@outlook.com>
2019-08-04 22:33:36 +0700

    Remove rechart and make reuse for react project

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 const { format } = require('date-fns')
 
-const date = format(new Date(), "YYYY-MM-DD [at] HH:mm")
-const data = [{name: date, uv: 400, pv: 2400, amt: 2400}]
-
-const renderLineChart = (
-  <LineChart width={600} height={300} data={data}>
-    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-    <CartesianGrid stroke="#ccc" />
-    <XAxis dataKey="name" />
-    <YAxis />
-  </LineChart>
-);
-
 const App = () => {
-  return <div>{renderLineChart}</div>
+  return <div>{format(new Date(), "YYYY-MM-DD [at] HH:mm")}</div>
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
